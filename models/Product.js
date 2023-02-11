ba4efbdbdded1b7c8ba7b67169ad4ed66a699140
Andrew Chou <andrewchou1996@gmail.com>
2023-02-10 16:07:53 -0800

    Finish models definitions and relations in ORM

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,50 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      // prevents null values
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      // prevents null values
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      // prevents null values
+      allowNull: false,
+      validate: {
+        // will only allow any numbers
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      // prevents null values
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+        // checks for valid integers
+        isInt: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,13 +4,51 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// {========== To create a One-To-Many relationship, the "hasMany" and "belongsTo" associations are used together ==========}
+// One(Category) - To - Many(Product)
+
 // Products belongsTo Category
+/**
+The A.belongsTo(B) association means that a One-To-One relationship
+exists between A and B, with the foreign key being defined in the source model (A).
+**/
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+/**
+The A.hasMany(B) association means that a One-To-Many relationship
+exists between A and B, with the foreign key being defined in the target model (B).
+**/
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+  onUpdate: 'SET NULL'
+});
 
+// {========== To create a Many-To-Many relationship, two belongsToMany calls are used together ==========}
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
